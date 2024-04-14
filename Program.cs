0e4041ee107bfd3f1baf564bd9f1ba1970ce12b1
hoanghaisc001 <69134435+hoanghaisc001@users.noreply.github.com>
2024-04-14 14:41:36 +0700

    Add Script
    Add Script

--- Program.cs
+++ Program.cs
@@ -1,23 +1,71 @@
-﻿internal class Program
+﻿using System.Drawing;
+using System.Security.Cryptography.X509Certificates;
+
+internal class Program
 {
+  
     private static void Main(string[] args)
     {
-         int[] aray01 = new int[8];
-    for (int i = 0; i < aray01.Length ; i++)
-    {
-        aray01[i] = i;
+      Array01 ar01 = new Array01();
+      ar01.P_Array();
+      
+      
     }
-    int sum03 = 0;
-    for (int i = 0; i < aray01.Length ; i++)
-    {
-       if (i < aray01.Length - 1)
-       Console.Write(aray01[i] + ",");
-       else Console.Write(aray01[i]);
+}
 
-       sum03 += aray01[i];
-    }
-    Console.WriteLine(" ");
-    Console.WriteLine("gia tri tong cua mang :" + sum03);
+public class Array01
+{
+    public int[] Sheep = new int[4];
+
+    public int[] tiger;
+
+    public string[] name = {"Long", "Minh","Dan","Kim"};
+    
+
+     public void P_Array()
+    {
+        Console.WriteLine("Nhap kich thuoc mang");
+
+        int size =0;
+        do 
+        {
+             size = Convert.ToInt32(Console.ReadLine());
+        }
+        while (size > 8);
+
+        int[] cloud = new int[size];
+
+        Console.WriteLine("Nhap gia tri cua mang");
+        for ( int i =0;i< cloud.Length;i++)
+        {
+            cloud[i] = int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine("in gia tri mang ");
+        for (int i =0; i< cloud.Length;i++)
+        {
+            Console.Write(cloud[i] + " , ");
+        }
+        
+        int sum = 0;
+        int max = 0;
+
+        Console.WriteLine("Tinh tong va tim gia tri lon nha cua mang");
+        for ( int j =0; j<cloud.Length; j++)
+        {
+            sum += cloud[j];
+            if ( max < cloud[j])
+            {
+                max = cloud[j];
+            }
+
+        }
+
+        Console.WriteLine(" tong cua mang la : " + sum);
+        Console.WriteLine("gia tri lon nhat la :" + max);
+        Console.WriteLine(cloud[3]);
+        Console.WriteLine(cloud[4]);
 
     }
 }
+
